47db031d55e67fbe0a273a6f842ac130a2e1649b
Apipilikas <apipilikas@gmail.com>
2022-01-07 18:35:13 +0200

    user case 1 finished

---
+++ script.js
@@ -0,0 +1,101 @@
+const url = "https://elearning-aueb.herokuapp.com/courses/search?title=";
+
+window.onload = init;
+
+
+var templates = {}
+
+templates.subjectNames = Handlebars.compile(`
+<h3>Αποτελέσματα με λέξη-κλειδί <span>{{keyword}}</span></h3>
+{{#if noResults}}
+<span class="alert-box">Δεν βρέθηκε μάθημα με αυτήν την λέξη-κλειδί.</span>
+{{/if}}
+{{#unless noResults}}
+<ol>
+    {{#each subjectNames}}
+    <li>{{this}}</li>
+    {{/each}}
+</ol>
+{{/unless}}
+`);
+
+
+
+function init(){
+    
+    const search_input = document.getElementById('search-input');
+    const search_btn = document.getElementById('search-btn');
+    const div = document.querySelector('#results-area-content');
+    const alertBox = document.querySelector('#search-area-content > .alert-box');
+    const letters = /[a-zA-ZΑ-Ωα-ω]/;
+    const comma = /[, ]/;
+    const constraints = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    // Example
+    // makeRequest(url, "python");
+
+    search_btn.onclick = function (e) {
+        var inptValue = search_input.value.trim();
+        console.log("Button clicked")
+        if (inptValue === ''){
+            showAlertBox("Εισάγεται μία λέξη-κλειδί στο πεδίο!");
+        }
+        else if (letters.test(inptValue) && comma.test(inptValue)) {
+            showAlertBox("Εισάγεται μόνο μία λέξη-κλειδί στο πεδίο!");
+        }
+        else if (constraints.test(inptValue)) {
+            showAlertBox("Εισάγεται μόνο γράμματα και αριθμούς!");
+        }
+        else {
+            alertBox.style.display = "none";
+            makeRequest(url, inptValue, div);
+        }
+    }
+
+    function showAlertBox(message) {
+        div.innerHTML = '';
+        alertBox.style.display = "initial";
+        alertBox.innerHTML = message;
+    }
+}
+
+function makeRequest(url, keyword, div){
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+
+    let init = {
+        method: "GET",
+        headers: myHeaders
+    }
+
+    fetch(url + keyword, init)
+    .then(response => {
+        if (response.status == 200) {
+            return response.json();
+        }
+        else {
+            console.log(">!< Looks like something went wrong :/ >!<");
+        }
+    })
+    .then (data => {
+
+        let subjectNamesData = {
+            "keyword":keyword,
+            "noResults":true,
+            "subjectNames":[]
+        }
+
+        for (sbjName of data){
+            subjectNamesData.subjectNames.push(sbjName.title)
+            subjectNamesData.noResults = false;
+        }
+
+        let subjectContent = templates.subjectNames(subjectNamesData);
+
+        div.innerHTML = subjectContent;
+
+        
+    })
+    .catch(error => {
+        console.log(">!< Fetch error >!<", error);
+    })
+}
